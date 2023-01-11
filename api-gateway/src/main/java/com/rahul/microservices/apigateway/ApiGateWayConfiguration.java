a3c6c332bf39b9742bdcd31f98554a315ccf6237
rahul-dot-class <rahul07969@gmail.com>
2023-01-12 00:45:44 +0530

    api gate Way first commit

---
+++ api-gateway/src/main/java/com/rahul/microservices/apigateway/ApiGateWayConfiguration.java
@@ -0,0 +1,22 @@
+package com.rahul.microservices.apigateway;
+
+import java.util.function.Function;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGateWayConfiguration {
+
+//	@Bean
+//	public RouteLocator getwayRouter(RouteLocatorBuilder builder ) {
+//		Function<PredicateSpec,Buildable<Route>> routeFunction =p-> p.path("/get").uri("http://httpbin.org:80");
+//		return builder.routes().route(routeFunction).build();
+//		
+//	}
+}
